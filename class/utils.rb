972a0722526ab28514800c1b3baeceb5a658ea5d
Yoshinori <gairoju@gmail.com>
2015-03-26 20:26:44 +0900

    Partly accelarated with C, and fixed a bug in samplin method

--- class/utils.rb
+++ class/utils.rb
@@ -1,11 +1,46 @@
 class Utils
+  require 'bundler/setup'
+  require 'inline'
+
+  inline do |builder|
+    builder.include('<math.h>')
+    builder.c_singleton'
+      double
+      log_c(double i){
+        return log(i);
+      }'
+  end
+
+  inline do |builder|
+    builder.include('<math.h>')
+    builder.c_singleton'
+      double
+      exp_c(double i){
+        return exp(i);
+      }'
+  end
+
+  inline do |builder|
+    builder.include('<math.h>')
+    builder.c_singleton'
+      double
+      log_sum_exp_c(double a, double b){
+        if(a == b)
+          return a + log(2);
+
+        if( a > b )
+          return a + log( 1 + exp(b-a) );
+        else
+          return b + log( 1 + exp(a-b) );
+      }'
+  end
 
   def self.sample_three_probs(log1, log2, log3)
     max = [log1,log2,log3].max
 
-    p1 = Math.exp(log1-max)
-    p2 = Math.exp(log2-max)
-    p3 = Math.exp(log3-max)
+    p1 = exp_c(log1-max)
+    p2 = exp_c(log2-max)
+    p3 = exp_c(log3-max)
 
     sum = p1+p2+p3
     p1 /= sum
@@ -17,9 +52,9 @@ class Utils
 
     if random < p1
       return 0
-    elsif p1 <= random && random < p2
+    elsif p1 <= random && random < p1+p2
       return 1
-    elsif p2 <= random
+    elsif p1+p2 <= random && random < p1+p2+p3
       return 2
     else
       return -1 #error check
@@ -28,18 +63,18 @@ class Utils
 
   def self.log_sum_exp(a, b, f = boolean)
     return b if f
-    return a + Math.log(2) if a == b
+    return a + log_c(2) if a == b
 
     if( a > b )
-      return a + Math.log( 1 + Math.exp(b-a) )
+      return a + log_c( 1 + exp_c(b-a) )
     else
-      return b + Math.log( 1 + Math.exp(a-b) )
+      return b + log_c( 1 + exp_c(a-b) )
     end
 
   end
 
   def self.log_sum_exp3(a, b, c)
-    return self.log_sum_exp(self.log_sum_exp(a, b, false), c, false)
+    return self.log_sum_exp_c(self.log_sum_exp_c(a, b), c)
   end
 
 =begin
@@ -62,3 +97,18 @@ class Utils
 =end
 
 end
+
+=begin
+# test code 
+if __FILE__ == $0
+  puts Utils.log_c()
+  puts Math.log(0.2)
+end
+
+# test for sampling
+if __FILE__ == $0
+  100.times do
+    puts Utils.sample_three_probs(-5.415927617733775, -4.724336573224139, Utils.log_c(0))
+  end
+end
+=end
