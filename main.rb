979a6d72dcf834c1014dbf0c008ec96d3d75d92a
Yoshinori <gairoju@gmail.com>
2015-04-27 16:07:26 +0900

    mainly changed drawing class.

--- main.rb
+++ main.rb
@@ -21,7 +21,8 @@ class MakePhylogenyPDF < Prawn::Document
     @root            = @pt.root
     @line_width      = 1
     @title_font_size = 20
-    @taxon_font_size = @title_font_size/2
+    @font_ratio      = 1.5
+    @taxon_font_size = @title_font_size/@font_ratio
 
     @leaf_offset = 350
     @y_offset    = 50
@@ -132,13 +133,24 @@ class MakePhylogenyPDF < Prawn::Document
         if @phylo_prof[leaf.name] == 1
           fill_color @stroke_color[2]
           stroke_color @base_stroke_color
-          fill_and_stroke_rectangle [@leaf_offset+@prof_offset, @node_pos[leaf][1]+@taxon_font_size], @taxon_font_size*2, @taxon_font_size*2
+          fill_and_stroke_rectangle [@leaf_offset+@prof_offset, @node_pos[leaf][1]+@taxon_font_size], @taxon_font_size*@font_ratio, @taxon_font_size*@font_ratio
         elsif @phylo_prof[leaf.name] == 2
           fill_color @stroke_color[3]
           stroke_color @base_stroke_color
-          fill_and_stroke_rectangle [@leaf_offset+@prof_offset, @node_pos[leaf][1]+@taxon_font_size], @taxon_font_size*2, @taxon_font_size*2
+          fill_and_stroke_rectangle [@leaf_offset+@prof_offset, @node_pos[leaf][1]+@taxon_font_size], @taxon_font_size*@font_ratio, @taxon_font_size*@font_ratio
+        elsif @phylo_prof[leaf.name] == 3
+          fill_color @stroke_color[2]
+          stroke_color @stroke_color[2]
+          fill_and_stroke_rectangle [@leaf_offset+@prof_offset, @node_pos[leaf][1]+@taxon_font_size], @taxon_font_size*@font_ratio/2, @taxon_font_size*@font_ratio
+
+          fill_color @stroke_color[3]
+          stroke_color @stroke_color[3]
+          fill_and_stroke_rectangle [@leaf_offset+@prof_offset+@taxon_font_size*@font_ratio/2, @node_pos[leaf][1]+@taxon_font_size], @taxon_font_size*@font_ratio/2, @taxon_font_size*@font_ratio
+
+          stroke_color @base_stroke_color
+          stroke_rectangle [@leaf_offset+@prof_offset, @node_pos[leaf][1]+@taxon_font_size], @taxon_font_size*@font_ratio, @taxon_font_size*@font_ratio
         else
-          stroke_rectangle [@leaf_offset+@prof_offset, @node_pos[leaf][1]+@taxon_font_size], @taxon_font_size*2, @taxon_font_size*2
+          stroke_rectangle [@leaf_offset+@prof_offset, @node_pos[leaf][1]+@taxon_font_size], @taxon_font_size*@font_ratio, @taxon_font_size*@font_ratio
         end
       end
     end
@@ -159,51 +171,63 @@ class MakePhylogenyPDF < Prawn::Document
 
         num = @pt.node2num[child]
 
-        if state_parent == 0
-          if child == @sgb[@gn]
-            stroke_color @stroke_color[3]
-            state = 2
-          elsif child == @gb[@gn]
-            stroke_color @stroke_color[2]
-            state = 1
-          else
-            stroke_color @stroke_color[1]
-            state = state_parent
-          end
-        elsif state_parent == 1
-
-          if child == @sgb[@gn]
-            stroke_color @stroke_color[3]
-            state = 2
-          else
-            if @bp[@gn][num][0] > 0.5
-              p10_hex = (255*@bp[@gn][num][0]).to_i.to_s(16)
-              p10_hex = p10_hex << "0000"
-              stroke_color p10_hex
-              state = 0
-            else
+        if !@pt.same_sub_tree[@gb[@gn]][child]
+          stroke_color @stroke_color[0]
+        else
+          if state_parent == 0
+            if child == @sgb[@gn]
+              stroke_color @stroke_color[3]
+              state = 2
+            elsif child == @gb[@gn]
               stroke_color @stroke_color[2]
+              state = 1
+            else
+              stroke_color @stroke_color[1]
               state = state_parent
             end
-          end
+          elsif state_parent == 1
 
-        elsif state_parent == 2
-          if @bp[@gn][num][1] > 0.33
-            p20_hex = (255*@bp[@gn][num][1]).to_i.to_s(16)
-            p20_hex = p20_hex << "0000"
-            stroke_color p20_hex
-            state = 0
-          elsif @bp[@gn][num][2] > 0.33
-            p21_hex = (255*@bp[@gn][num][2]).to_i.to_s(16)
-            p21_hex = "0000" << p21_hex
-            stroke_color p21_hex
-            state = 1
+            if child == @sgb[@gn]
+              stroke_color @stroke_color[3]
+              state = 2
+            else
+              if @bp[@gn][num][0] > 0.5
+                p10_hex = (255*@bp[@gn][num][0]).to_i.to_s(16)
+                p10_hex = p10_hex << "0000"
+                stroke_color p10_hex
+                state = 0
+              else
+                stroke_color @stroke_color[2]
+                state = state_parent
+              end
+            end
+
+          elsif state_parent == 2
+            if @bp[@gn][num][1] > 0.33
+              p20_hex = (255*@bp[@gn][num][1]).to_i.to_s(16)
+              p20_hex = p20_hex << "0000"
+              stroke_color p20_hex
+              state = 0
+            elsif @bp[@gn][num][2] > 0.33
+              p21_hex = (255*@bp[@gn][num][2]).to_i.to_s(16)
+              p21_hex = "0000" << p21_hex
+              stroke_color p21_hex
+              state = 1
+            else
+              stroke_color @stroke_color[3]
+              state = 2
+            end
           else
-            stroke_color @stroke_color[3]
-            state = 2
+            if @sgb[@gn] == child
+              stroke_color @stroke_color[3]
+              state = 2
+            elsif @gb[@gn] == child
+              stroke_color @stroke_color[2]
+              state = 1
+            else
+              STDERR.puts "Error: unknown state is specified. #{state_parent}"
+            end
           end
-        else
-          STDERR.puts "Error: unknown state is specified. #{state_parent}"
         end
 
         #if _is_signal_gained?(child)
@@ -219,10 +243,13 @@ class MakePhylogenyPDF < Prawn::Document
 
         stroke_color @base_stroke_color
 
+        draw_text "#{@pt.node2num[r]}", :at => [@node_pos[r][0],@node_pos[r][1]-@taxon_font_size/2], :size => @taxon_font_size
+
       end
 
     else
-      draw_text r.name, :at => [@node_pos[r][0],@node_pos[r][1]-@taxon_font_size/2]
+      draw_text "#{@pt.node2num[r]}", :at => [@node_pos[r][0]-@taxon_font_size*2, @node_pos[r][1]-@taxon_font_size/2], :size => @taxon_font_size
+      draw_text r.name, :at => [@node_pos[r][0],@node_pos[r][1]-@taxon_font_size/2], :size => @taxon_font_size
     end
 
   end
@@ -235,14 +262,14 @@ class MakePhylogenyPDF < Prawn::Document
     end
 
     stroke_color @base_stroke_color
-    stroke_axis
+    #stroke_axis
 
     text "Estimation of presequence evolution with MitoFates", :size => @title_font_size, :color=>"009900"
     draw_text "Phylogenetic profile", :at => [@leaf_offset+@prof_offset, cursor-@title_font_size]
 
     current_y_pos = cursor
 
-    span = _calc_span(current_y_pos)
+    span = _calc_span(current_y_pos)/8
 
     current_y_pos -= @y_offset
 
@@ -309,7 +336,9 @@ if __FILE__ == $0
   input_genes = pp.symbol2index.keys
   ih = InferHist.new(pt, pp, input_genes)
   ih.pre_process()
-  ih.mcmc(1000,400)
+
+  #ih.mcmc(30000,3000)
+  ih.mcmc(30,6)
 
   input_genes.each do |gene|
     taxon2prof = Hash.new()
@@ -321,7 +350,12 @@ if __FILE__ == $0
     end
   
     mp = MakePhylogenyPDF.new(pt, gene, ih.gain_branch, ih.signal_gain_branch, ih.hidden_states, ih.branch_params, taxon2prof)
-    mp.render_file("figs/#{gene}.pdf")
+
+    #mp.render_file("figs_yeast_primitive_mts/#{gene}.pdf")
+    #mp.render_file("figs/#{gene}.pdf")
+    #mp.render_file("figs_yeast_rbh/#{gene}.pdf")
+    mp.render_file("test/#{gene}.pdf")
+    #mp.render_file("#{gene}.pdf")
   end
 
   exit 0
